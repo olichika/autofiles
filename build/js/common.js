af91c0c61dacafbdc6d1c5ea547fb2b7bf6087ac
Olga <parankinaolya@gmail.com>
2018-10-09 13:16:25 +0300

    'commit'

---
+++ build/js/common.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+
+	$('.main__automotive-news_concept1').click(function(){
+		$(this).toggleClass('main__automotive-news_detail1').next().toggle();
+		return false;
+	})
+
+	$('.main__automotive-news_concept2').click(function(){
+		$(this).toggleClass('main__automotive-news_detail2').next().toggle();
+		return false;
+	})
+
+	$('.main__automotive-news_concept3').click(function(){
+		$(this).toggleClass('main__automotive-news_detail3').next().toggle();
+		return false;
+	})
+
+	$('.main__automotive-news_concept4').click(function(){
+		$(this).toggleClass('main__automotive-news_detail4').next().toggle();
+		return false;
+	})
+
+	$('.main__automotive-news_concept5').click(function(){
+		$(this).toggleClass('main__automotive-news_detail5').next().toggle();
+		return false;
+	})
+
+	$('.main__automotive-news_concept6').click(function(){
+		$(this).toggleClass('main__automotive-news_detail6').next().toggle();
+		return false;
+	})
+
+	$('.main__accessories-type_btn1').click(function(){
+		$(this).hide().toggleClass('.main__accessories-type_open').next().toggle();
+		return false;
+	})
+
+	$('.main__accessories-type_btn2').click(function(){
+		$(this).hide().toggleClass('.main__accessories-type_open').next().toggle();
+		return false;
+	})
+
+	$('.main__accessories-type_btn3').click(function(){
+		$(this).hide().toggleClass('.main__accessories-type_open').next().toggle();
+		return false;
+	})
+
+	$('.main__accessories-type_btn4').click(function(){
+		$(this).hide().toggleClass('.main__accessories-type_open').next().toggle();
+		return false;
+	})
+
+	$('.main__accessories-type_btn5').click(function(){
+		$(this).hide().toggleClass('.main__accessories-type_open').next().toggle();
+		return false;
+	})
+
+	$('.main__accessories-type_btn6').click(function(){
+		$(this).hide().toggleClass('.main__accessories-type_open').next().toggle();
+		return false;
+	})
+
+	$('.main__accessories-type_btn7').click(function(){
+		$(this).hide().toggleClass('.main__accessories-type_open').next().toggle();
+		return false;
+	})
+
+	$('.main__accessories-type_btn8').click(function(){
+		$(this).hide().toggleClass('.main__accessories-type_open').next().toggle();
+		return false;
+	})
+
+	$('.main__accessories-type_btn9').click(function(){
+		$(this).hide().toggleClass('.main__accessories-type_open').next().toggle();
+		return false;
+	})
+
+});
